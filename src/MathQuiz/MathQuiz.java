46112e90992516f536a99cb24db9ca86799dc163
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-06-23 21:36:42 +0200

    MathQuiz program perhaps takes answers from user but the there is a problem in putting the answers from user to the machine.

---
+++ src/MathQuiz/MathQuiz.java
@@ -0,0 +1,30 @@
+package MathQuiz;
+
+import static java.lang.Math.pow;
+
+public class MathQuiz {
+    double a;
+    double b;
+    double c;
+
+    public MathQuiz(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public MathQuiz() {
+    }
+
+    boolean questionOne(double a) {
+        return 3 * 5 == a;
+    }
+
+    boolean questionTwo(double b) {
+        return pow(12, 2) == b;
+    }
+
+    boolean questionThree( double c){
+        return pow(15129, 2) == c;
+    }
+}
