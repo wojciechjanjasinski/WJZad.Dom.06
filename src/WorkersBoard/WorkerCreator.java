2b7f40192ef97d06ce6485ea2f34e27974c35f4c
Wojciech Jasinski <wojtek.jasinski@poczta.fm>
2020-06-23 14:27:05 +0200

    I can't run this program properly.

---
+++ src/WorkersBoard/WorkerCreator.java
@@ -0,0 +1,22 @@
+package WorkersBoard;
+
+import java.util.Scanner;
+
+public class WorkerCreator {
+    public Worker createWorker() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj prosze imię pracownika: ");
+        String firstName = scanner.nextLine();
+        scanner.nextLine();
+
+        System.out.print("Podaj prosze nazwisko pracownika: ");
+        String lastName = scanner.nextLine();
+        scanner.nextLine();
+
+        System.out.print("Podaj proszę wysokość wynagrodzenia ");
+        double salary = scanner.nextDouble();
+        scanner.nextLine();
+        return new Worker(firstName, lastName, salary);
+    }
+
+}
--- src/WorkersBoard/WorkersBoard.java
+++ src/WorkersBoard/WorkersBoard.java
@@ -1,10 +1,16 @@
 package WorkersBoard;
 
 public class WorkersBoard {
-    String[] workersBoard = new String[3];
-    Worker worker = new Worker();
+    public static void main(String[] args) {
+        WorkerCreator workerCreator = new WorkerCreator();
+        String[] workersBoard = new String[3];
+        workersBoard[0] = workerCreator.createWorker().getFirstName() + workerCreator.createWorker().getLastName() + workerCreator.createWorker().getSalary();
+        workersBoard[1] = workerCreator.createWorker().getFirstName() + workerCreator.createWorker().getLastName() + workerCreator.createWorker().getSalary();
+        workersBoard[2] = workerCreator.createWorker().getFirstName() + workerCreator.createWorker().getLastName() + workerCreator.createWorker().getSalary();
+        System.out.println(workersBoard[0]);
+        System.out.println(workersBoard[1]);
+        System.out.println(workersBoard[2]);
 
-    public String[] getWorkersBoard() {
-        return workersBoard;
+
     }
 }
